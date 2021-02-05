4da233b0767bd999ebb7e19ac1906b67952b3d57
koketama <60101042+koketama@users.noreply.github.com>
2021-02-05 16:31:52 +0800

    mk protoc-gen entrypoint.sh

---
+++ build/protoc-gen/entrypoint.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+mkdir -p api gen
+
+protoc -I/usr/local/include -I. --proto_path=. --go_out=gen --go-grpc_out=gen --grpc-gateway_out=logtostderr=true:gen --openapiv2_out=logtostderr=true:api
