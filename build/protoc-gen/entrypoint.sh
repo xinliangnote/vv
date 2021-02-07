b62d10957668ade090c3ecfcf18feb4a62c188be
koketama <60101042+koketama@users.noreply.github.com>
2021-02-07 17:29:32 +0800

    enable message validator

--- build/protoc-gen/entrypoint.sh
+++ build/protoc-gen/entrypoint.sh
@@ -5,6 +5,7 @@ mkdir -p api gen
 protoc -I/usr/local/include -I. --proto_path=. \
 --go_out=gen \
 --go-grpc_out=gen \
+--message-validator_out=gen \
 --grpc-gateway_out=logtostderr=true:gen \
 --openapiv2_out=logtostderr=true:api \
 *.proto
